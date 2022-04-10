46ff7b5e7548506376b845abb58769e47783b51c
Abdoul Razac SANE <abdoulrazacsane@gmail.com>
2022-04-10 13:32:59 +0200

    ff

--- API/Tests/test_create_db.py
+++ API/Tests/test_create_db.py
@@ -1,28 +1,31 @@
 import datetime
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Boolean, create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import relationship
 
 import datetime 
-
+from sqlalchemy.ext.declarative import declared_attr
 Base = declarative_base()
 
-class ClasseBase(Base):
+class ClasseBase(object):
+    @declared_attr
+    def __tablename__(cls):
+        return cls.__name__.lower()
     id = Column(Integer, primary_key=True)
     create_at  = Column(DateTime, default = datetime.datetime.utcnow())
     update_at  = Column(DateTime, default = datetime.datetime.utcnow())
 
 
-class User(ClasseBase):
+class User(ClasseBase, Base):
     __tablename__ = "users"
     user_name = Column(String(30))
     password = Column(String(30))
     nom = Column(String(30))
     prenom = Column(String(30))
-    tel = Column(Int(10))
-    user_type = Column(bool)
-    date_naissance = Column(Date)
-    trash = Column(bool)
+    tel = Column(Integer)
+    user_type = Column(Boolean)
+    date_naissance = Column(DateTime)
+    trash = Column(Boolean)
     email = Column(String(70))
 
 
--- API/db/Base.py
+++ API/db/Base.py
@@ -6,6 +6,7 @@ class BaseModel(object):
     @declared_attr
     def __tablename__ (cls):
         return cls.__name__.lower()
+        
     id = Column(Integer, primary_key=True)
     create_at  = Column(DateTime, default = datetime.datetime.utcnow())
     update_at  = Column(DateTime, default = datetime.datetime.utcnow())
